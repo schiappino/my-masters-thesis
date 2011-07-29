9c6d552b8f59b21fdffea4bf0246d6fb8b03e189
netholik <netholik@d5417a9b-353e-50d0-132f-3a12f4a86f55>
2011-07-29 12:46:06 +0000

    * some clean up in the code + program now runs in two modes (video file or webcam) + added classifiers for eyes and moouth + added detection of eyes in restricted ROI based on facial constrains

--- OpenCV sample/Sample 3/Sample 3.cpp
+++ OpenCV sample/Sample 3/Sample 3.cpp
@@ -1,6 +1,3 @@
-// Sample 3.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include <cv.h>
@@ -8,63 +5,95 @@
 #include <highgui.h>
 #include <time.h>
 
+#define PROGRAM_MODE 10
+
 void detect_and_draw( IplImage* img );
 
+
+// ********************************** CASCADE FILES ******************************************
+const char* cascade_fname_eye = "../data/haarcascade_eye_alt.xml";
+const char* cascade_fname_face = "../data/haarcascade_frontalface_alt.xml";
+const char* cascade_fname_mouth = "../data/haarcascade_mcs_mouth.xml";
+//const char* cascade_fname_face = "../data/haarcascades/haarcascade_mcs_nose.xml";
+
+// *********************************** VIDEO FILES ******************************************
+const char* video_fname = "C:/Users/netholik/Videos/240/Video_00006.avi";
+
+
+// ****************************** GLOBALS ***************************************************
+static CvCapture				*capture		= NULL;
+static CvMemStorage				*storage		= NULL;
+static CvHaarClassifierCascade	*cascade_face	= NULL,
+								*cascade_eye	= NULL,
+								*cascade_mouth	= NULL;
+
+
+CvPoint		point1, 
+			point2;
+
+CvSeq		*faces			= NULL,
+			*eyes			= NULL,
+			*mouth			= NULL;
+
+IplImage	*frame			= NULL,
+			*RawFrame		= NULL,
+			*resizedFrame	= NULL,
+			*grayFrame		= NULL;
+
+time_t		start;
+time_t		end;
+
+int			counter		= 0,
+			posRes		= 0,
+			object_size = 0,
+			scale		= 1;
+
+double		fps = 0, 
+			sec = 0;
+
+char		text[3];
 const char*	window_name = "OpenCV";
-//const char* cascade_name = "C:/Program Files/OpenCV/data/haarcascades/haarcascade_eye.xml";
-const char* cascade_name = "C:/Program Files/OpenCV/data/haarcascades/haarcascade_frontalface_alt.xml";
-//const char* cascade_name = "C:/Program Files/OpenCV/data/haarcascades/haarcascade_mcs_nose.xml";
-
-
-static CvMemStorage* storage = NULL;
-static CvHaarClassifierCascade* cascade = NULL;
-
-int scale = 1;
-// Create two points to represent the face locations
-CvPoint pt1, pt2;
-
-CvSeq* faces = NULL;
-
-time_t start, end;
-double fps;
-int counter = 0,
-	posRes = 0;
-double sec;
-char fpsCstring[3];
 
 
 
 using namespace cv;
 
+//****************** MAIN ENTRY HERE ***************************
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int percent = 50;
-
 	cvNamedWindow( window_name, CV_WINDOW_AUTOSIZE );
 
-	CvCapture* capture = NULL;
-	//capture = cvCreateCameraCapture(CV_CAP_ANY);
-	capture = cvCreateFileCapture( "C:/Users/netholik/Videos/240/Video_00006.avi" );
-    if(!capture)
+	if( PROGRAM_MODE == 1 )
+	{
+		capture = cvCreateCameraCapture(CV_CAP_ANY);
+		
+		cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH,  240);
+		cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 180);
+	}
+	else
+	{
+		capture = cvCreateFileCapture( video_fname );
+	}
+
+    
+	if(!capture)
     {
-        std::cout << "Dupa" << std::endl;
+        std::cout << "Capture could not be initialized properly" << std::endl;
         return -1;
     }	
 
-	/*cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH,  240);
-	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 180);*/
+	
 
 
-	IplImage* frame = NULL,
-			*RawFrame = NULL,
-			*resizedFrame = NULL,
-			*grayFrame = NULL;
+
 
 	// Allocate the memory storage
-	storage = cvCreateMemStorage(0);
-	cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
+	storage			= cvCreateMemStorage(0);
+	cascade_face	= (CvHaarClassifierCascade*)cvLoad( cascade_fname_face, 0, 0, 0 );
+	cascade_eye		= (CvHaarClassifierCascade*)cvLoad( cascade_fname_eye, 0, 0, 0 );
+	cascade_mouth	= (CvHaarClassifierCascade*)cvLoad( cascade_fname_mouth, 0, 0, 0 );
 	
-	if( !cascade )
+	if( !cascade_face || !cascade_eye || !cascade_mouth )
 	{
 		fprintf( stderr, "ERROR: Could not load classifier cascade\n" );
 		return -1;
@@ -77,13 +106,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 
-	char *cvLibraries; 
-	char *cvModules; 
-	cvGetModuleInfo(0, (const char**) &cvLibraries, (const char**) &cvModules); 
-	printf("OpenCV Libraries: %s\nOpenCV Modules: %s\n", cvLibraries, cvModules); 
- 
-
-
 	frame = cvQueryFrame( capture );
 	if( !frame ) return -1;
 
@@ -91,9 +113,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	CvSize frameSize = cvSize(frame->width, frame->height);
 	CvSize orgFrameSize = frameSize;
-
-	frameSize.height *= percent / 100;
-	frameSize.height *= percent / 100;
 	grayFrame = cvCreateImage ( orgFrameSize, IPL_DEPTH_8U, 1);
 
 
@@ -116,21 +135,25 @@ int _tmain(int argc, _TCHAR* argv[])
 		++counter;
 		sec = difftime (end, start);     
 		fps = counter / sec;
-		//printf("FPS = %.1f\n", fps);
-		_gcvt(fps, 3, fpsCstring);
-		cvPutText(grayFrame, fpsCstring, cvPoint(10, 20), &font, cvScalar(255, 255, 255, 0));
+		_gcvt(fps, 3, text);
+		cvPutText(grayFrame, text, cvPoint(10, 20), &font, cvScalar(255, 255, 255, 0));
+		
+		_gcvt(object_size, 3, text);
+		cvPutText(grayFrame, text, cvPoint(10, 40), &font, cvScalar(255, 255, 255, 0));
+
 		cvShowImage( window_name, grayFrame);
 
 
 		char c = cvWaitKey(1);
 		if( c == 27 ) break;
 	}
-
 	time(&end);
-	std::cout	<< "Total effectiveness estemated " << ((double) posRes / (double) counter) * 100 << std::endl
-				<< "Total exec time was " << end - start <<  std::endl;
 
-	cvReleaseMemStorage(&storage);
+	std::cout	<< "Total effectiveness estemated " << ((double) posRes / (double) counter) * 100 << std::endl
+				<< "Total exec time was " << end - start <<  std::endl
+				<< "Avarage FPS was " << (double)counter / (double)(end - start) << std::endl;
+
+	cvReleaseMemStorage( &storage );
 	cvReleaseCapture( &capture );
 	cvDestroyAllWindows();
 }
@@ -146,11 +169,11 @@ void detect_and_draw( IplImage* img )
 	// There can be more than one face in an image. So create a growable sequence of faces.
 	// Detect the objects and store them in the sequence
 	faces = cvHaarDetectObjects( img,
-								cascade, 
+								cascade_face, 
 								storage,
-								1.7, 2, 
+								1.3, 2, 
 								CV_HAAR_FIND_BIGGEST_OBJECT,
-								cvSize(40, 40) );
+								cvSize(30, 30) );
 
 	// Loop the number of faces found.
 	for( i = 0; i < (faces ? faces->total : 0); i++ )
@@ -159,20 +182,48 @@ void detect_and_draw( IplImage* img )
 		CvRect* r = (CvRect*)cvGetSeqElem( faces, i );
 
 		// Find the dimensions of the face,and scale it if necessary
-		pt1.x = r->x*scale;
-		pt2.x = (r->x+r->width)*scale;
-		pt1.y = r->y*scale;
-		pt2.y = (r->y+r->height)*scale;
+		point1.x = r->x*scale;
+		point2.x = (r->x+r->width)*scale;
+		point1.y = r->y*scale;
+		point2.y = (r->y+r->height)*scale;
 
-		printf("Frame %d\t\tFace coords: %d\t %d\t %d\t %d\n", counter, pt1.x, pt1.y, pt2.x, pt2.y);
+		object_size = point2.x - point1.x;
+		printf("Frame %d\tsize %d\tcoords: %d\t %d\t %d\t %d\n", counter, object_size, point1.x, point1.y, point2.x, point2.y);
 
 		// Draw the rectangle in the input image
-		cvRectangle( img, pt1, pt2, CV_RGB(255,255,0), 2, 8, 0 );
+		cvRectangle( img, point1, point2, CV_RGB(255,255,0), 2, 8, 0 );
 
 		// increamet found objects count
 		++posRes;
 	}
 
+	// Clear mem for next detection
+	cvClearMemStorage(storage);
+
+	CvRect *r = (CvRect*)cvGetSeqElem(faces, 0);
+	cvSetImageROI(img, cvRect(r->x, r->y + (r->height/5.5), r->width, r->height/3.0));
+
+    /* detect eyes */
+	CvSeq* eyes = cvHaarDetectObjects( 
+					img, 
+					cascade_eye, 
+					storage,
+					1.15, 3, 
+					CV_HAAR_DO_CANNY_PRUNING,
+					cvSize(25, 15));
+
+    /* draw a rectangle for each eye found */
+	for( i = 0; i < (eyes ? eyes->total : 0); i++ ) 
+	{
+		r = (CvRect*)cvGetSeqElem( eyes, i );
+		cvRectangle(img, 
+					cvPoint(r->x, r->y), 
+					cvPoint(r->x + r->width, r->y + r->height),
+					CV_RGB(255, 0, 0), 1, 8, 0);
+	}
+
+    cvResetImageROI(img);
+	eyes = NULL;
 	faces = NULL;
 }
 

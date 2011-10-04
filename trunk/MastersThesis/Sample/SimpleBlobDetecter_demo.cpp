52a7165ca991e5b51488100e7118187c6a3b8878
NetHolik@gmail.com <NetHolik@gmail.com@d5417a9b-353e-50d0-132f-3a12f4a86f55>
2011-10-04 13:42:05 +0000

    + added simple blob detector as by extending OpenCV SimpleBlobDetector class with methods detectEx, findBlobsEx, detectImplEx

--- trunk/MastersThesis/Sample/SimpleBlobDetecter_demo.cpp
+++ trunk/MastersThesis/Sample/SimpleBlobDetecter_demo.cpp
@@ -15,29 +15,50 @@ int main()
 	const char	* wndName = "Source image",
 				* wndNameGray = "Gray img", 
 				* wndNameOut = "Out",
-				* filename = "../data/images/eyes-exp-transform.png";
+				* filename = "../data/images/eyebrows-smoothed.bmp";
 
 	Mat src, gray, thresh, binary;
 	Mat out;
 	vector<KeyPoint> keyPoints;
+	vector< vector <Point>> contours;
+	vector< vector <Point>> approxContours;
 
 	SimpleBlobDetector::Params params;
-	params.minThreshold = 10;
-	params.blobColor = 255;
+	params.minThreshold = 40;
+	params.maxThreshold = 60;
+	params.thresholdStep = 5;
+
+	params.minArea = 100; 
+	params.minConvexity = 0.3;
+	params.minInertiaRatio = 0.01;
+
+	params.maxArea = 8000;
+	params.maxConvexity = 10;
+
+	params.filterByColor = false;
+	params.filterByCircularity = false;
 
 	namedWindow( wndNameOut, CV_GUI_NORMAL );
-	//namedWindow( wndName, CV_GUI_NORMAL );
 
 	src = imread( filename, CV_LOAD_IMAGE_GRAYSCALE );
+	line( src, Point(0, src.rows-1), Point( src.cols-1, src.rows-1 ), Scalar::all(255) );
 
-	SimpleBlobDetector blobDetector;
+	SimpleBlobDetector blobDetector( params );
 	blobDetector.create("SimpleBlob");
 
 	for(;;)
 	{
+		//blobDetector.detect( src, keyPoints );
+		blobDetector.detectEx( src, keyPoints, contours );
+		drawKeypoints( src, keyPoints, out, CV_RGB(0,255,0), DrawMatchesFlags::DEFAULT);
+		approxContours.resize( contours.size() );
 
-		blobDetector.detect( src, keyPoints );
-		drawKeypoints( src, keyPoints, out, CV_RGB(0,255,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+		for( int i = 0; i < contours.size(); ++i )
+		{
+			approxPolyDP( Mat(contours[i]), approxContours[i], 4, 1 );
+			drawContours( out, contours, i, CV_RGB(rand()&255, rand()&255, rand()&255) );
+			drawContours( out, approxContours, i, CV_RGB(rand()&255, rand()&255, rand()&255) );
+		}
 		cout << "Keypoints " << keyPoints.size() << endl;
 	
 		imshow( wndNameOut, out );

2cea45651c5e22e15be3fd2865e831f52a66dd04
netholik@gmail.com <netholik@gmail.com@d5417a9b-353e-50d0-132f-3a12f4a86f55>
2011-11-12 21:25:18 +0000

    FacialFeaturesValidation: eye position metric based on inter ocular distance - printing statistical values on console

--- trunk/MastersThesis/FacialFeatures/Eyes.h
+++ trunk/MastersThesis/FacialFeatures/Eyes.h
@@ -2,5 +2,7 @@
 #include "FacialFeatures.h"
 
 void DetectEyes(void);
-void EyeTemplateMatching( Mat src, Mat disp, Mat templ, int irisRadius);
+Point EyeTemplateMatching( Mat src, Mat disp, Mat templ, int irisRadius);
 void DrawGroundTruthEyePos();
+inline double getInterocularDist( FacialFeaturesValidation& features );
+void eyePositionsMetric( Point& left, Point& right, FacialFeaturesValidation& features );
